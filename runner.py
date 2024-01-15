9ca775ba47bbe6fe0a16b6bf5559f3a79054c401
Yuki Sakamoto <sakamotoyuki.jpn+gh@gmail.com>
2024-01-15 20:35:38 +0900

    try to integrate objective1 and objective2. However, it has not yet run properly

--- runner.py
+++ runner.py
@@ -64,13 +64,13 @@ mlflc = MLflowCallback(
     metric_name="sum of square x_mean and y_mean",
 )
 
-def generate_objective_function(generation_output_path):
+def generate_objective_function(generation_output_path, eval_weight = {"transmat_rss": 0.5, "startprob": 0.5, "D": 0.0}):
     # This function returns the closure that hold the generation_output_path.
 
     @mlflc.track_in_mlflow()
     def _objective(trial):
         generation_output = generation_output_path  
-        mlflow.log_param("generation_output_path", generation_output)
+        mlflow.log_param("generation_output_path", generation_output)   #XXX should be use log_artifact ???
 
         # The parameters get from trial.suggest_XXX(XXX=float, int, ...) are automatically changed to optimize.
         analysis_params_mod = analysis_params.copy()
@@ -78,13 +78,13 @@ def generate_objective_function(generation_output_path):
         analysis_params_mod["overlap"] = trial.suggest_float("overlap", 0.1, 1.0)
         analysis_params_mod["max_sigma"] = trial.suggest_float("max_sigma", 4, 10)
         analysis_params_mod["min_sigma"] = trial.suggest_float("min_sigma", 0, 2)
+        analysis_params_mod["cutoff_distance"] = trial.suggest_float("cutoff_distance", 1, 10)  #XXX obj2
 
-        #analysis_output=Path('./outputs_analysis_run/'+str(trial_number))
-        analysis_output=Path('./outputs_analysis_run/'+str(trial.number))
-        analysis_output.mkdir(parents=True, exist_ok=True)
-        a,b = analysis1([generation_output], analysis_output, analysis_params_mod)
+        analysis1_output=Path('./outputs_analysis_run/'+str(trial.number) + '/analysis1/')
+        analysis1_output.mkdir(parents=True, exist_ok=True)
+        a, b = analysis1([generation_output], analysis1_output, analysis_params_mod)
 
-        evaluation_output=Path('./outputs_evaluation_run/'+str(trial_number))
+        evaluation_output=Path('./outputs_analysis_run/'+str(trial_number) + '/evaluation1/')
         evaluation_output.mkdir(parents=True, exist_ok=True)
 
         c,d = evaluation1([generation_output,analysis_output], evaluation_output, evaluation_params)
@@ -93,7 +93,34 @@ def generate_objective_function(generation_output_path):
         y_mean = d["y_mean"]
         mlflow.log_metric("x_mean", x_mean)
         mlflow.log_metric("y_mean", y_mean)
-        result = (x_mean)**2+(y_mean)**2
+        mean_norm2 = (x_mean)**2+(y_mean)**2
+        mlflow.log_metric("mean_norm2", mean_norm2)
+
+        # analysis2
+        analysis2_output = Path('./outputs_analysis_run/'+str(trial.number) + '/analysis2/')
+        analysis2_output.mkdir(parents=True, exist_ok=True)
+        analysis2_artifacts, analysis2_metrics = analysis2([generation_output], analysis1_output, analysis2_output, analysis_params_mod )
+
+        evaluation_output=Path('./outputs_analyais_run/'+str(trial_number) + '/evaluation2/')
+        evaluation_output.mkdir(parents=True, exist_ok=True)
+        _, metrics = evaluation2([generation_output, analysis2_output], evaluation_output, evaluation_params)
+
+        start_ratio = np.array(generation_params["Nm"]) / sum(generation_params["Nm"])
+        startprob_rss = np.sum(np.square(analysis2_metrics["startprob"] - start_ratio) )
+        analysis_Dm = np.sort(np.array(list(map(lambda x: x[0], analysis2_metrics["D"]) ) ))
+        generation_Dm = np.sort(np.array(generation_params["Dm"]))
+        dm_rss = np.sum(np.square(analysis_Dm - generation_Dm) ) 
+
+        objective_parameters = {
+            "startprob": startprob_rss,
+            "D": dm_rss,
+            "transmat_rss": metrics["transmat_rss"]
+        }
+        result = 0.
+        for k in eval_weight.keys():
+            print("{} : {} : {}".format(k, eval_weight[k], objective_parameters[k]))
+            result += objective_parameters[k] * eval_weight[k]
+        mlflow.log_artifacts(Path('./outputs_analysis_run/'+str(trial.number) ))
         return result
 
     return _objective
@@ -156,8 +183,15 @@ def objective2(generation_output_path, best_analysis1_trial_number ):
 
 
 if __name__ == '__main__':
-    analysis1_best_trial_number = 33
-    skip_generation = False
+    if len(sys.argv) > 1 and sys.argv[1] == '-d':
+        optuna.delete_study(
+            storage="sqlite:///example2.10.1.db", 
+            study_name="test_x_y_mean_storage2_7_2.10.1")
+        optuna.delete_study(
+            storage="sqlite:///example2.10.1.db", 
+            study_name="test_x_y_mean_storage2_7_2.10.1")
+        exit()
+
     generation_output = Path('./test_8frame/')
     if generation_output.exists() and (generation_output / "config.yaml").exists():
         print("Generation skip",file = sys.stderr )
@@ -167,7 +201,6 @@ if __name__ == '__main__':
         print("Generation start",file = sys.stderr )
         artifacts,metrics = generation1([], generation_output, generation_params)
         print("Generation done",file = sys.stderr )
-
 
     study = optuna.create_study(
             storage="sqlite:///example2.10.1.db", 
@@ -188,18 +221,6 @@ if __name__ == '__main__':
     fig.write_html('study.html')
 
     #objective2(generation_output, analysis1_best_trial_number)
-    study2 = optuna.create_study(
-            storage="sqlite:///example2.10.1.db", 
-            study_name="test_x_y_mean_storage2_7_2.10.1", 
-            load_if_exists=True, 
-            sampler=optuna.samplers.CmaEsSampler())
-
-    objective2 = generate_objective2_function(
-            generation_output, 
-            Path('./outputs_analysis_run/'+str(analysis1_best_trial_number)), 
-            {"transmat_rss": 0.5, "startprob": 0.5, "D": 0.2}
-    )
-    study2.optimize(objective2, n_trials=20, callbacks=[mlflc])
-    fig = optuna.visualization.plot_optimization_history(study2)
-    fig.write_html('study2.html')
+    fig = optuna.visualization.plot_optimization_history(study)
+    fig.write_html('study.html')
 
